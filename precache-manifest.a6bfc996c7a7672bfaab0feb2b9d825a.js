8da8b686d5fe6d5ab067c95a81af369e785f80e5
longdara <35132906+longdara@users.noreply.github.com>
2020-05-13 12:21:54 -0700

    Updates

---
+++ precache-manifest.a6bfc996c7a7672bfaab0feb2b9d825a.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b37ff019ace8aec63e6a2aeee9bfb10",
+    "url": "/dara-brokerage/index.html"
+  },
+  {
+    "revision": "2d57f224201e2d5954f3",
+    "url": "/dara-brokerage/static/css/10.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "f991a62f9af484d48cd1",
+    "url": "/dara-brokerage/static/css/11.1d9a2034.chunk.css"
+  },
+  {
+    "revision": "7a95f11f3b9d51099309",
+    "url": "/dara-brokerage/static/css/12.1d9a2034.chunk.css"
+  },
+  {
+    "revision": "6580255a521b1e0fa83c",
+    "url": "/dara-brokerage/static/css/2.21d3525d.chunk.css"
+  },
+  {
+    "revision": "a5e340082918d3473d89",
+    "url": "/dara-brokerage/static/css/6.3b4990d0.chunk.css"
+  },
+  {
+    "revision": "8f233918f5220165c501",
+    "url": "/dara-brokerage/static/css/7.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "bb183782b6162fe55e74",
+    "url": "/dara-brokerage/static/css/8.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "0a8218af2309f9f814b4",
+    "url": "/dara-brokerage/static/css/9.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "bfc95513d380046b10b4",
+    "url": "/dara-brokerage/static/css/main.8390ce8f.chunk.css"
+  },
+  {
+    "revision": "94471ccb6276ebbe59e3",
+    "url": "/dara-brokerage/static/js/0.ea93546e.chunk.js"
+  },
+  {
+    "revision": "5051e70ede3031d939a4",
+    "url": "/dara-brokerage/static/js/1.7d1caf62.chunk.js"
+  },
+  {
+    "revision": "2d57f224201e2d5954f3",
+    "url": "/dara-brokerage/static/js/10.9c15011e.chunk.js"
+  },
+  {
+    "revision": "f991a62f9af484d48cd1",
+    "url": "/dara-brokerage/static/js/11.bebd709d.chunk.js"
+  },
+  {
+    "revision": "7a95f11f3b9d51099309",
+    "url": "/dara-brokerage/static/js/12.69c965d9.chunk.js"
+  },
+  {
+    "revision": "4da167366c0c5aec6195",
+    "url": "/dara-brokerage/static/js/13.a2a45760.chunk.js"
+  },
+  {
+    "revision": "6000c891d7fcc1de3295",
+    "url": "/dara-brokerage/static/js/14.7f22c6b2.chunk.js"
+  },
+  {
+    "revision": "6580255a521b1e0fa83c",
+    "url": "/dara-brokerage/static/js/2.2b57acaf.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/dara-brokerage/static/js/2.2b57acaf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0dd0a78754f70dd93c7",
+    "url": "/dara-brokerage/static/js/3.60bc2df6.chunk.js"
+  },
+  {
+    "revision": "362cd232e6b400756e3bc1723069a8cd",
+    "url": "/dara-brokerage/static/js/3.60bc2df6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5e340082918d3473d89",
+    "url": "/dara-brokerage/static/js/6.680bf81f.chunk.js"
+  },
+  {
+    "revision": "8e01edd47da9aa824272b7ab763bdf7f",
+    "url": "/dara-brokerage/static/js/6.680bf81f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f233918f5220165c501",
+    "url": "/dara-brokerage/static/js/7.a09b375f.chunk.js"
+  },
+  {
+    "revision": "bb183782b6162fe55e74",
+    "url": "/dara-brokerage/static/js/8.9b724170.chunk.js"
+  },
+  {
+    "revision": "0a8218af2309f9f814b4",
+    "url": "/dara-brokerage/static/js/9.6c6880d5.chunk.js"
+  },
+  {
+    "revision": "bfc95513d380046b10b4",
+    "url": "/dara-brokerage/static/js/main.13dc63e4.chunk.js"
+  },
+  {
+    "revision": "3eb1362fbcf68e475ae3",
+    "url": "/dara-brokerage/static/js/runtime-main.0efa1cda.js"
+  },
+  {
+    "revision": "8c20481d522a1bf8c654d2fcb5d769b2",
+    "url": "/dara-brokerage/static/media/bribe.8c20481d.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/dara-brokerage/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/dara-brokerage/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/dara-brokerage/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/dara-brokerage/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/dara-brokerage/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
